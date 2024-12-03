fa503cc519df27ea33250f9a31aaa95620fb4ea1
xkcd576 <157149917+xkcd576@users.noreply.github.com>
2024-12-03 17:36:10 -0500

    Create page.tsx

---
+++ src/main_page/page.tsx
@@ -0,0 +1,9 @@
+
+
+// import BodyMessage from "@/src/body-message";
+
+export default function MainPage() {
+  return (
+    <p>Hi there! IDK what I am doing, but if you can see this, I'm somehow doing it correctly.</p>
+    )
+}
