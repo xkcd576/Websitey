74184ff86450ee379e86b92458d63d79cd3e29e7
xkcd576 <157149917+xkcd576@users.noreply.github.com>
2024-12-12 14:09:37 -0500

    Update page.tsx

--- src/main_page/page.tsx
+++ src/main_page/page.tsx
@@ -1,9 +1,16 @@
 
 
 // import BodyMessage from "@/src/body-message";
-
+/*
 export default function MainPage() {
   return (
     <p>Hi there! IDK what I am doing, but if you can see this, I'm somehow doing it correctly.</p>
     )
 }
+*/
+const button = document.getElementById('change-text-button') as HTMLButtonElement;
+const paragraph = document.getElementById('text-content') as HTMLParagraphElement;
+
+// Add an event listener to the button that changes the text of the paragraph
+button.addEventListener('click', () => {
+    paragraph.textContent = "The content has been updated!";
