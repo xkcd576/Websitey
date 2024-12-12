3c2bd8d876a0b69aee1fcc4188216c2eb0321c93
xkcd576 <157149917+xkcd576@users.noreply.github.com>
2024-12-12 14:08:34 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 
+const button = document.getElementById('change-text-button') as HTMLButtonElement;
+const paragraph = document.getElementById('text-content') as HTMLParagraphElement;
+
+// Add an event listener to the button that changes the text of the paragraph
+button.addEventListener('click', () => {
+    paragraph.textContent = "The content has been updated!";
